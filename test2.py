2cb8b630c7a4f2cff417156ad7baf5c0df0ed621
none <none@none.com>
2018-11-18 16:30:44 +0800

    Fix public key generation error

---
+++ test2.py
@@ -0,0 +1,6 @@
+from pyeoskit import wallet
+psw = wallet.create('mywallet6')
+print(psw)
+wallet.import_key('mywallet6', '5JEcwbckBCdmji5j8ZoMHLEUS8TqQiqBG1DRx1X9DN124GUok9s')
+wallet.import_key('mywallet6', '5JbDP55GXN7MLcNYKCnJtfKi9aD2HvHAdY7g8m67zFTAFkY1uBB')
+
