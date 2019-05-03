90019279637c5cd41f73c4e69e7045e402fe8d28
learnforpractice <learnforpractice@gmail.com>
2019-05-03 11:14:59 +0800

    add default abi

--- main/eosapi.py
+++ main/eosapi.py
@@ -11,6 +11,87 @@ from . import config
 _eosapi = _hello._eosapi
 wallet = _hello.wallet
 
+default_abi = '''
+{
+   "version": "eosio::abi/1.0",
+   "types": [{
+      "new_type_name": "account_name",
+      "type": "name"
+   }],
+  "structs": [{
+      "name": "transfer",
+      "base": "",
+      "fields": [
+        {"name":"from", "type":"account_name"},
+        {"name":"to", "type":"account_name"},
+        {"name":"quantity", "type":"asset"},
+        {"name":"memo", "type":"string"}
+      ]
+    },{
+     "name": "create",
+     "base": "",
+     "fields": [
+        {"name":"issuer", "type":"account_name"},
+        {"name":"maximum_supply", "type":"asset"}
+     ]
+  },{
+     "name": "issue",
+     "base": "",
+     "fields": [
+        {"name":"to", "type":"account_name"},
+        {"name":"quantity", "type":"asset"},
+        {"name":"memo", "type":"string"}
+     ]
+  },{
+      "name": "account",
+      "base": "",
+      "fields": [
+        {"name":"balance", "type":"asset"}
+      ]
+    },{
+      "name": "currency_stats",
+      "base": "",
+      "fields": [
+        {"name":"supply", "type":"asset"},
+        {"name":"max_supply", "type":"asset"},
+        {"name":"issuer", "type":"account_name"}
+      ]
+    }
+  ],
+  "actions": [{
+      "name": "transfer",
+      "type": "transfer",
+      "ricardian_contract": ""
+    },{
+      "name": "issue",
+      "type": "issue",
+      "ricardian_contract": ""
+    }, {
+      "name": "create",
+      "type": "create",
+      "ricardian_contract": ""
+    }
+
+  ],
+  "tables": [{
+      "name": "accounts",
+      "type": "account",
+      "index_type": "i64",
+      "key_names" : ["currency"],
+      "key_types" : ["uint64"]
+    },{
+      "name": "stat",
+      "type": "currency_stats",
+      "index_type": "i64",
+      "key_names" : ["currency"],
+      "key_types" : ["uint64"]
+    }
+  ],
+  "ricardian_clauses": [],
+  "abi_extensions": []
+}
+'''
+
 class Function(object):
     def __init__(self, function):
         self.function = function
@@ -18,7 +99,10 @@ class Function(object):
 
     def __call__(self, *args):
         ret = self.function(*args)
-        return JsonStruct(ret)
+        try:
+            return JsonStruct(ret)
+        except json.JSONDecodeError:
+            return ret
 
 class GetAccountFunction(object):
     def __init__(self, function):
@@ -228,8 +312,14 @@ class EosApi(object):
         abi = db.get_abi(account)
         if not abi:
             abi = self.client.get_abi(account)
-            abi = json.dumps(abi['abi'])
-            db.set_abi(account, abi)
+            if abi and 'abi' in abi:
+                abi = json.dumps(abi['abi'])
+                db.set_abi(account, abi)
+            else:
+                abi = ''
+                db.set_abi(account, abi)
+        if not abi:
+            abi = default_abi
         return abi
 
     def pack_args(self, account, action, args):
