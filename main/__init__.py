759475d4d35fae99e5c8296a2a965b1911ad6c9e
learnforpractice <learnforpractice@gmail.com>
2019-06-07 08:50:01 +0800

    Add version

--- main/__init__.py
+++ main/__init__.py
@@ -7,7 +7,7 @@ from .http_client import HttpClient
 from .client import Client, WalletClient
 from . import _hello as hello
 from . import eosapi
-
+__version__='0.6.0'
 wallet = _hello.wallet
 eosapi = eosapi.EosApi()
 
