53ee6c1785a8ff9cd3006792664c44b4d4c2c608
learnforpractice <learnforpractice@gmail.com>
2020-01-14 10:53:29 +0800

    Update wasmcompiler.py

--- main/wasmcompiler.py
+++ main/wasmcompiler.py
@@ -9,7 +9,7 @@ import tempfile
 def find_eosio_cdt_path():
     eosio_cpp = shutil.which('eosio-cpp')
     if not eosio_cpp:
-        raise "eosio.cdt not installed, please refer to https://github.com/eosio/eosio.cdt for an installation guide"
+        raise Exception("eosio.cdt not installed, please refer to https://github.com/eosio/eosio.cdt for an installation guide")
     eosio_cpp = os.path.realpath(eosio_cpp)
     eosio_cpp = os.path.dirname(eosio_cpp)
     return os.path.dirname(eosio_cpp)
