a12481a10216e9cb9471214c94685a3ca37e7b8b
learnforpractice <learnforpractice@gmail.com>
2020-11-21 04:27:09 +0800

    Testcase for compile

---
+++ pytest/test_mp.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import time
+import pytest
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(lineno)d %(module)s %(message)s')
+logger=logging.getLogger(__name__)
+test_dir = os.path.dirname(__file__)
+
+
+from pyeoskit import eosapi
+
+class Test(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def setup_method(self, method):
+        pass
+
+    def teardown_method(self, method):
+        pass
+
+    def test_compile(self):
+        for i in range(10000):
+            code = eosapi.compile("print('hello,world')")
+            assert code
