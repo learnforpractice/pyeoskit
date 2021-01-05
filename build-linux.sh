c8cd025bebba80d588d3424037a3aa2a656050a2
learnforpractice <learnforpractice@gmail.com>
2021-01-05 13:01:03 +0800

    Update build scripts

--- build-linux.sh
+++ build-linux.sh
@@ -1,2 +1,2 @@
-CC=clang CXX=clang++ python3.7 setup.py sdist bdist_wheel  -- -DCMAKE_TOOLCHAIN_FILE=$(pwd)/cmake/polly/clang-fpic.cmake -- -j7
+CC=clang CXX=clang++ python3.7 setup.py sdist bdist_wheel  -- -DCMAKE_TOOLCHAIN_FILE=$(pwd)/cmake/polly/clang-fpic.cmake -- -j$(nproc)
 
