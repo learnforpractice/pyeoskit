bda19d78064a4990a212f31a2a2180ef0adb9b9a
learnforpractice <learnforpractice@gmail.com>
2021-01-08 17:10:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-python3 -m pip uninstall uuoskit -y;python3 -m pip install ./dist/uuoskit-0.8.1-cp37-cp37m-macosx_10_9_x86_64.whl 
+python3 -m pip uninstall uuoskit -y;python3 -m pip install ./dist/uuoskit-0.8.2-cp37-cp37m-macosx_10_9_x86_64.whl
 
