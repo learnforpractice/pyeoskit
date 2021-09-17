36935a61b680b0e334ad29079d45ebf2dd9d5efb
learnforpractice <learnforpractice@gmail.com>
2021-09-17 20:47:53 +0800

    Update submodule

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 python3 src/check_modification.py src/uuoskit
-CC=clang CXX=clang++ python3 setup.py sdist bdist_wheel --plat-name macosx-10.9-x86_64 -- -- -j$(sysctl -n hw.logicalcpu)
+CC=clang CXX=clang++ python3 setup.py sdist bdist_wheel
 if [ $? -eq 0 ]; then
 ./install.sh
 fi
