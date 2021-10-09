e4a9d117af3362172e0711e03db3bdcdd878ac30
learnforpractice <learnforpractice@gmail.com>
2021-10-09 20:00:16 +0800

    Update version to v1.0.1

--- pysrc/__init__.py
+++ pysrc/__init__.py
@@ -5,7 +5,7 @@ from .rpc_interface import RPCInterface, WalletClient
 from .chainapi_sync import ChainApi
 from uuoskit import _uuoskit
 
-__version__='1.0.0'
+__version__='1.0.1'
 
 _uuoskit.init()
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import glob
 
 setup(
     name="uuoskit",
-    version="1.0.0",
+    version="1.0.1",
     description="Python Toolkit for EOSIO",
     author='The UUOSIO Team',
     license="MIT",
