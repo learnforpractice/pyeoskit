07006085bee20255e97870f62552e801b41effdb
learnforpractice <learnforpractice@gmail.com>
2022-03-07 15:24:33 +0800

    Small fix

--- pysrc/__init__.py
+++ pysrc/__init__.py
@@ -3,7 +3,7 @@ import sys
 from .http_client import HttpClient
 from .rpc_interface import RPCInterface, WalletClient
 from .chainapi_sync import ChainApi
-from pyeoskit import _pyeoskit
+from . import _pyeoskit
 
 __version__='1.1.5'
 
