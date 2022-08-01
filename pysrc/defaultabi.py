514f1b9afdcf0ebe9dd1c0340d1ef68611e1799d
learnforpractice <learnforpractice@gmail.com>
2022-08-01 11:58:48 +0800

    Fix cp950 decode exception

--- pysrc/defaultabi.py
+++ pysrc/defaultabi.py
@@ -3,10 +3,10 @@ import os
 cur_dir = os.path.dirname(__file__)
 
 eosio_token_abi = None
-with open(os.path.join(cur_dir, 'data/eosio.token.abi'), 'r') as f:
+with open(os.path.join(cur_dir, 'data/eosio.token.abi'), 'rb') as f:
     eosio_token_abi = f.read()
 
-with open(os.path.join(cur_dir, 'data/eosio.system_eosio.abi'), 'r') as f:
+with open(os.path.join(cur_dir, 'data/eosio.system_eosio.abi'), 'rb') as f:
     eosio_system_abi_eosio = f.read()
 
 eosio_system_abi = dict(
